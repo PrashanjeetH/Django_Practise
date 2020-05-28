36d47dddd6e73fa48317222b3e68bf3a63479b54
Prashanjeet <jeet8halder@gmail.com>
2020-05-28 06:10:48 +0530

    Deleted Practse4

--- practise4/entryForm/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-#define your urls here
-
-urlpatterns = [
-    path("", views.index, name = "index"),
-    path("", views.page1, name = "page1"),
-]
