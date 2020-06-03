89d95e32609336d9b0dfb8a7d5ed265569fc3a8e
Prashanjeet <jeet8halder@gmail.com>
2020-06-03 12:24:26 +0530

    selenium project

---
+++ practise7/CBV/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
+from django.core.files.storage import FileSystemStorage
+from django.urls import reverse_lazy
+
+from .forms import BookForm
+from .models import Book
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {
+        'books': books
+    })
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'class_book_list.html'
+    context_object_name = 'books'
+
+
+class UploadBookView(CreateView):
+    model = Book
+    # fields = ["title", "author"]  # these are two ways of defining modelforms in Django
+    form_class = BookForm
+    success_url = reverse_lazy('class_book_list')
+    template_name = 'upload_book.html'
+
+
+"""def upload(request):
+    if request.method == 'POST':
+    context = {}
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html', context)
+"""
+
+
+"""
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+        return render(request, 'upload_book.html', {
+        'form': form
+        })
+"""
+
+"""def delete_book(request, pk):
+if request.method == 'POST':
+    book = Book.objects.get(pk=pk)
+    book.delete()
+return redirect('book_list')
+"""
