3ed3a0017439cb391943d50c284a02f7c2467d77
Prashanjeet <jeet8halder@gmail.com>
2020-09-14 13:14:57 +0530

    Uppdated SMTP settings

--- practise10/practise10/settings.py
+++ practise10/practise10/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'jb!12l8+@-uvsnriw92-0n6gs&9wb&vt7la5m)uy+08s=ggu!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
