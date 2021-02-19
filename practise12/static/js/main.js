f3a1e72a28a5f7faf66b5f5242a86ca6da24df73
PrashanjeetH <jeet8halder@gmail.com>
2021-02-19 21:11:47 +0530

    requirements.txt

--- practise12/static/js/main.js
+++ practise12/static/js/main.js
@@ -1,23 +1,69 @@
-var className = document.getElementsByClassName('fetchClass')
 
-function fetchData(){
-  console.log('Requesting for resources...')
 
-  var url = '/fetch/'
 
-  fetch(url, {
-    method: 'POST',
-    headers:{
-      'Content-Type':'application/json',
-    },
-    body:JSON.stringify({'PrashanjeetData in body'})
-  })
 
-  .then((response) =>{
-    return response.json()
-  })
 
-  .then((data) =>{
-    console.log('data:', data)
-  })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var className = document.getElementsByClassName('fetchClass')
+//
+// function fetchData(){
+//   console.log('Requesting for resources...')
+//
+//   var url = '/fetch/'
+//
+//   fetch(url, {
+//     method: 'POST',
+//     headers:{
+//       'Content-Type':'application/json',
+//     },
+//     body:JSON.stringify({'PrashanjeetData in body'})
+//   })
+//
+//   .then((response) =>{
+//     return response.json()
+//   })
+//
+//   .then((data) =>{
+//     console.log('data:', data)
+//   })
+// }
